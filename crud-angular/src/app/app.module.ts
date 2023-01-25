558529ae35a677c6c0d9c2661b4d474f688fce1d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-25 20:55:14 -0300

    added: mat table component

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
+
 @NgModule({
   declarations: [
     AppComponent
