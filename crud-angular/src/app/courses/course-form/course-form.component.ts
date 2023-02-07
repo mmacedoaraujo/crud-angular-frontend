a5a516395f7af98c92ab7f04ffbba62697bf273b
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-02-07 10:45:07 -0300

    implemented: create new course operation on backend and frontend

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
@@ -13,7 +14,7 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private service: CoursesService, private snackBar: MatSnackBar) {
+  constructor(private formBuilder: FormBuilder, private service: CoursesService, private snackBar: MatSnackBar, private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -25,11 +26,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => { this.onError() });
+      .subscribe(result => this.onSuccess(), error => this.onError());
+      this.onCancel()
   }
 
   onCancel() {
-    console.log('alou')
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open("Course created successfully!", "close", { duration: 3000 })
   }
 
   private onError() {
